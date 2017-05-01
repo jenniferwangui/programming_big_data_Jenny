a64e5c688da150f86405ce8de756869a5e045913
Jennifer Mwangi <Jennifer Mwangi>
2017-05-01 23:33:31 +0100

    CA4 and CA5 uploaded

---
+++ CA5/Calculator.R
@@ -0,0 +1,80 @@
+# 1)Addation function 
+
+Addation <- function(value1, value2){
+  func <- value1 + value2
+  return(func)
+}
+Addation(8, -6)
+
+# 2)Subtraction function
+
+Subtraction <- function(value1, value2){
+  func <- value1 - value2
+  return(func)
+}
+Subtraction(20, -6)
+
+# 3)Multiplication function
+
+Multiplication <- function(value1, value2){
+  func <- value1 * value2
+  return(func)
+}
+Multiplication(20, -6)
+
+# 4)Division function
+
+Division <- function(value1, value2){
+  func <- value1 / value2
+  return(func)
+}
+Division(36, -6)
+
+# 5)Exponetial function
+
+Exponetial <- function(value1, value2){
+  func <- value1 ** value2
+  return(func)
+}
+Exponetial(4, 2)
+
+# 6)squareroot function
+
+Squareroot <- function(value){
+  func <- value ** (1.0/2.0)
+  return(func)
+}
+Squareroot(9)
+
+# 7)squared function
+
+Squared <- function(value){
+  func <- value * value
+  return(func)
+}
+Squared(9)
+
+# 8)squareroot function
+
+cuberoot <- function(value){
+  func <- value ** (1.0/3.0)
+  return(func)
+}
+cuberoot(64)
+
+# 9)Cos function
+
+Cos2 <- function(value){
+  
+  func <- cos(value)
+  return(func)
+}
+Cos2(90)
+
+# 10) sin function
+Sin2 <- function(value){
+  
+  func <- sin(value)
+  return(func)
+}
+Sin2(95)
